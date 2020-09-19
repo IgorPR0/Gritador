664a100fd45438312f1c995a6ef8d0ff21f647c4
igor matos ribeiro <igortecnologia2020@gmail.com>
2020-09-18 21:19:49 -0300

    Feito atividade Gritador

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string qualquerCoisa;
+
+            //Inicio(Escreva)//
+            Console.Write("Escreva qualquer coisa: ");
+            qualquerCoisa = Console.ReadLine();
+            Console.Write("\n");
+
+            Console.WriteLine($"{qualquerCoisa}!!!".ToUpper());
+            //Final(Escreva)//
+
+            //Partida(Sair)//
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+            //Final(Sair)//
+        }
+    }
+}
